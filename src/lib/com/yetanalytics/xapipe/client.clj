a5b81921cb476f8b878b1aa1c8b3af17a8bf9faf
Milt Reder <milt@yetanalytics.com>
2021-10-07 13:59:57 -0400

    more nuanced approach, working until

--- src/lib/com/yetanalytics/xapipe/client.clj
+++ src/lib/com/yetanalytics/xapipe/client.clj
@@ -3,17 +3,24 @@
   (:require #_[org.httpkit.client :as client]
             [clj-http.client :as client]
             [clojure.core.async :as a]
-            [clojure.java.io :as io]
             [clojure.spec.alpha :as s]
             [com.yetanalytics.xapipe.client.multipart-mixed :as multipart]
             [xapi-schema.spec :as xs]
-            [xapi-schema.spec.resources :as xsr]))
+            [xapi-schema.spec.resources :as xsr]
+            [xapipe.util.time :as t]))
+
+;; TODO: Pick and enable actual loggings
+(defn- debug
+  "DEV logging"
+  [& args]
+  (apply println "DEBUG: " args))
 
 ;; Add multipart-mixed output coercion
 (defmethod client/coerce-response-body :multipart/mixed
   [_ resp]
   (multipart/parse-response resp))
 
+;; Config needed for all requests
 (s/def ::url-base
   string?)
 
@@ -31,6 +38,44 @@
             ::username
             ::password]))
 
+;; Allow the user to pass in a subset of xAPI params to limit/filter data
+;;
+;; notably absent are attachments and ascending, which are default true
+;;
+;; since will be used initially but then overwritten when iterating
+;; it is always assumed to be present and will be set to the epoch if not
+;;
+;; until will cause the transfer to terminate TODO: this
+
+(s/def ::get-params
+  (s/keys
+   :opt-un [:xapi.statements.GET.request.params/since
+            :xapi.statements.GET.request.params/until
+            :xapi.statements.GET.request.params/limit
+            :xapi.statements.GET.request.params/agent
+            :xapi.statements.GET.request.params/verb
+            :xapi.statements.GET.request.params/activity
+            :xapi.statements.GET.request.params/registration
+            :xapi.statements.GET.request.params/related_activities
+            :xapi.statements.GET.request.params/related_agents
+            :xapi.statements.GET.request.params/format]))
+
+(def epoch-stamp "1970-01-01T00:00:00Z")
+
+(defn- escape-get-params
+  [params]
+  (select-keys params
+               [:since
+                :until
+                :limit
+                :agent
+                :verb
+                :activity
+                :registration
+                :related_activities
+                :related_agents
+                :format]))
+
 (def get-request-base
   {:headers      {"x-experience-api-version" "1.0.3"}
    :method       :get
@@ -39,14 +84,13 @@
                   :attachments true}})
 
 (s/def ::more string?) ;; more link
-(s/def ::since ::xs/timestamp) ;; since arg
-(s/def ::limit pos-int?)
+#_(s/def ::since ::xs/timestamp) ;; since arg
+#_(s/def ::limit pos-int?)
 
 (s/fdef get-request
   :args (s/cat :config ::request-config
-               :kwargs (s/keys* :opt-un [::more
-                                         ::since
-                                         ::limit]))
+               :params ::get-params
+               :more   (s/? ::more))
   :ret map?)
 
 (defn get-request
@@ -56,35 +100,27 @@
            username
            password]
     :or   {xapi-prefix "/xapi"}}
-   & {?since :since
-      ?more  :more
-      ?limit :limit}]
-  (let [more? (not-empty ?more)]
-    (cond-> (merge
-             get-request-base
-             {:url
-              (if more?
-                (format "%s%s"
-                        url-base
-                        ?more)
-                (format "%s%s/statements"
-                        url-base
-                        xapi-prefix))})
-      ;; support basic auth if provided
-      (and (not-empty username)
-           (not-empty password))
-      (assoc :basic-auth [username password])
-      ;; query params will be included in the more link
-      more?
-      (dissoc :query-params)
+   get-params
+   & [?more]]
+  (cond-> (if (not-empty ?more)
+            ;; Using More Link
+            (-> get-request-base
+                (assoc :url
+                       (format "%s%s"
+                               url-base
+                               ?more))
+                (dissoc :query-params))
+            (-> get-request-base
+                (assoc :url
+                       (format "%s%s/statements"
+                               url-base
+                               xapi-prefix))
+                (update :query-params merge get-params)))
 
-      (and (not more?)
-           ?since)
-      (assoc-in [:query-params :since] ?since)
-
-      (and (not more?)
-           ?limit)
-      (assoc-in [:query-params :limit] ?limit))))
+    ;; support basic auth if provided
+    (and (not-empty username)
+         (not-empty password))
+    (assoc :basic-auth [username password])))
 
 (def post-request-base
   {:headers {"x-experience-api-version" "1.0.3"}
@@ -160,30 +196,30 @@
   nat-int?)
 
 (s/fdef get-chan
-  :args (s/cat :config ::request-config
-               :poll-interval ::poll-interval
+  :args (s/cat :out-chan any?
                :stop-chan any?
-               :kwargs
-               (s/keys* :opt-un [::more
-                                 ::since
-                                 ::limit]))
+               :config ::request-config
+               :init-params ::get-params
+               :poll-interval ::poll-interval)
   :ret any?)
 
 (defn get-chan
   "Returns a channel that will return responses from an LRS forever or until it
   returns an error and closes or recieves a signal on stop-chan.."
-  [config
-   poll-interval
+  [out-chan
    stop-chan
-   & req-kwargs]
-  (let [out-chan (a/chan)]
-    (a/go-loop [req (apply
-                     get-request
-                     config
-                     req-kwargs)
-                since (some-> req
-                              :query-params
-                              :since)]
+   config
+   {init-since :since
+    ?until     :until
+    :as        init-params
+    :or        {init-since epoch-stamp}}
+   poll-interval]
+  (let [init-req (get-request
+                  config
+                  init-params)]
+    (a/go-loop [req   init-req
+                since init-since]
+      (debug 'req req 'since since)
       (let [req-chan (async-request
                       req)
             [v p]    (a/alts! [req-chan stop-chan])]
@@ -195,42 +231,62 @@
           (let [[tag resp :as ret] v]
             (case tag
               :response
-              (let [{{{:strs [statements more]} :statement-result} :body} resp]
-                (if (not-empty statements)
-                  (let [last-stored (-> statements
-                                        peek
-                                        (get "stored"))]
-                    ;; put on the (unbuffered) channel. Will park
-                    (a/>! out-chan
-                          (assoc-in ret [1 ::last-stored] last-stored))
-                    (recur
-                     ;; A more link gives us am automatic way to continue
-                     (if (not-empty more)
-                       (get-request
-                        config
-                        :more more)
-                       (get-request
-                        config
-                        :since last-stored))
-                     ;; update
-                     last-stored))
-                  ;; With no statements we're polling.
-                  ;; Wait for the specified time
+              (let [{{consistent-through "X-Experience-API-Consistent-Through"}
+                     :headers
+                     {{:strs [statements more]} :statement-result}
+                     :body}      resp
+                    ?last-stored (some-> statements
+                                         peek
+                                         (get "stored"))]
+                ;; If there are statements, emit them before continuing.
+                ;; This operation will park for takers.
+                (when (not-empty statements)
+                  (debug (format "emitting %d statements, last stored is %s"
+                                 (count statements)
+                                 ?last-stored))
+                  (a/>! out-chan
+                        (assoc-in ret [1 ::last-stored] ?last-stored)))
+                (cond
+                  ;; If the more link indicates there are more statements to
+                  ;; provide, immediately attempt to get them.
+                  (not-empty more) (do
+                                     (debug
+                                      (format "more link %s found, redispatching"
+                                              more))
+                                     (recur
+                                      (get-request
+                                       config
+                                       {} ;; has no effect with more
+                                       more)
+                                      ;; Set since for the cursor if we have new stuff
+                                      (or ?last-stored since)))
+
+                  ;; The lack of a more link means we are at the end of what the
+                  ;; LRS has for the given query.
+                  ;; At this point we check for an until and maybe terminate
+                  (and ?until
+                       (not= 1
+                             (t/stamp-cmp
+                              ?until
+                              consistent-through)))
                   (do
-                    (a/<! (a/timeout poll-interval))
-                    (recur
-                     (cond
-                       (not-empty more) (get-request
-                                         config
-                                         :more more)
-                       since            (get-request
-                                         config
-                                         :since since)
-                       :else            (apply
-                                         get-request
-                                         config
-                                         req-kwargs))
-                     since))))
+                    (debug
+                     (format "terminating because %s consistent-through is equal or later than %s until"
+                             consistent-through ?until))
+                    (a/close! out-chan))
+                  ;; With no more link or until, we're polling.
+                  ;; Wait for the specified time
+                  :else (do
+                          (debug (format "waiting %d ms..." poll-interval))
+                          (a/<! (a/timeout poll-interval))
+                          (recur
+                           (get-request
+                            config
+                            ;; Update Since to the LRS header
+                            (assoc init-params
+                                   :since
+                                   consistent-through))
+                           consistent-through))))
               :exception
               (do (a/>! out-chan ret)
                   (a/close! out-chan)))))))
@@ -238,14 +294,18 @@
 
 (comment
 
-  (def stop-chan (a/promise-chan))
+  (do
+    (def out-chan (a/chan))
+    (def stop-chan (a/promise-chan))
 
-  (def gchan
-    (get-chan
-     {:url-base    "http://localhost:8080"
-      :xapi-prefix "/xapi"}
-     1000
-     stop-chan))
+    (def gchan
+      (get-chan
+       out-chan
+       stop-chan
+       {:url-base    "http://localhost:8080"
+        :xapi-prefix "/xapi"}
+       {#_#_:until "2021-10-06T16:59:07.429354000Z"}
+       1000)))
 
   (first (a/<!! gchan)) ;; will block when out of statements!
 
@@ -254,16 +314,17 @@
   (-> (client/request
        (get-request
         {:url-base    "http://localhost:8080"
-         :xapi-prefix "/xapi"})
+         :xapi-prefix "/xapi"}
+        {})
        )
-      :body
-      :attachments
+      :headers
       )
 
   (-> (async-request
        (get-request
         {:url-base    "http://localhost:8080"
-         :xapi-prefix "/xapi"})
+         :xapi-prefix "/xapi"}
+        {})
        )
       a/<!!
       first
@@ -278,7 +339,8 @@
           :keys [attachments]} :body
          :as                   get-result} (client/request
                                             (get-request
-                                             req-config))
+                                             req-config
+                                             {}))
 
         ;; Post
         post-resp (client/request
---
+++ src/lib/com/yetanalytics/xapipe/util/time.clj
@@ -0,0 +1,24 @@
+(ns xapipe.util.time
+  "Time(stamp) utilities"
+  (:require [clojure.spec.alpha :as s]
+            [xapi-schema.spec :as xs])
+  (:import [java.time Instant]))
+
+(s/fdef parse-inst
+  :args (s/cat :timestamp ::xs/timestamp)
+  :ret #(instance? Instant %))
+
+;; TODO: This is primitive, will probably need timestamp norm stuff from LRS
+(defn parse-inst
+  ^Instant [^String timestamp]
+  (Instant/parse timestamp))
+
+(s/fdef stamp-cmp
+  :args (s/cat :a string?
+               :b string?)
+  :ret #{-1 0 1})
+
+(defn stamp-cmp
+  [a b]
+  (compare (parse-inst a)
+           (parse-inst b)))
