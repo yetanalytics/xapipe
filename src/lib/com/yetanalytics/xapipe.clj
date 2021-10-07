b19e6b5361d58fbeed433b5097b3c4db17c27d63
Milt Reder <milt@yetanalytics.com>
2021-10-07 15:00:29 -0400

    prep main lib ns

--- src/lib/com/yetanalytics/xapipe.clj
+++ src/lib/com/yetanalytics/xapipe.clj
@@ -1,7 +1,13 @@
 (ns com.yetanalytics.xapipe
-  (:require [clojure.spec.alpha :as s])
-  (:gen-class))
+  (:require [clojure.spec.alpha :as s]
+            [com.yetanalytics.xapipe.client :as client]
+            [clojure.core.async :as a]
+            [com.yetanalytics.xapipe.job :as job]))
 
+(s/def ::job
+  job/job-spec)
+
+;; TODO: delete boiler
 (defn greet
   "Callable entry point to the application."
   [data]
