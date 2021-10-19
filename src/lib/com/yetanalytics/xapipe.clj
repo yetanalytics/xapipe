e11b910035135b88a49ddec70f8dd3b3452d8277
Milt Reder <milt@yetanalytics.com>
2021-10-19 16:52:36 -0400

    Added typed stop signal and short-circuit

--- src/lib/com/yetanalytics/xapipe.clj
+++ src/lib/com/yetanalytics/xapipe.clj
@@ -4,6 +4,7 @@
             [com.yetanalytics.xapipe.client :as client]
             [com.yetanalytics.xapipe.event :as event]
             [com.yetanalytics.xapipe.job :as job]
+            [com.yetanalytics.xapipe.job.state.errors :as errors]
             [com.yetanalytics.xapipe.store :as store]
             [com.yetanalytics.xapipe.util.time :as t]
             [com.yetanalytics.xapipe.xapi :as xapi]))
@@ -14,11 +15,13 @@
 (s/def ::event
   event/event-spec)
 
-(s/fdef run-job
-  :args (s/cat :store #(satisfies? store/XapipeStore %)
-               :id ::job/id
-               :init-config ::job/config)
-  :ret (s/fspec :args (s/cat) :ret ::job))
+(s/def :com.yetanalytics.xapipe.stop-signal/status
+  #{:paused
+    :error})
+
+(s/def ::stop-signal
+  (s/keys :req-un [:com.yetanalytics.xapipe.stop-signal/status]
+          :opt-un [::errors/error]))
 
 (s/fdef run-job
   :args (s/cat :store #(satisfies? store/XapipeStore %)
@@ -47,13 +50,13 @@
                                  :job  job-before}))
       ;; set up channels and start
       (let [stop-chan (a/promise-chan)
-            stop-fn   #(a/put! stop-chan true)
+            stop-fn   #(a/put! stop-chan {:status :paused})
             {{{:keys [poll-interval]
                {?query-since :since
                 :as get-params} :get-params
                get-req-config :request-config
                :as            source-config} :source
-              {;; target-batch-size :batch-size
+              {target-batch-size :batch-size
                post-req-config   :request-config
                :as               target-config} :target} :config} job-before
             ;; Derive a since point for the query
@@ -64,7 +67,18 @@
             get-chan (client/get-chan
                       (a/chan
                        100 ;; TODO: configurable buffer
-                       (map event/get->event))
+                       (comp
+                        (map (fn [[tag x]]
+                               (case tag
+                                 :response x
+                                 :exception (throw x))))
+                        (mapcat xapi/response->statements)
+                        (partition-all target-batch-size))
+                       (fn [ex]
+                         (a/put! stop-chan {:status :error
+                                            :error {:message (ex-message ex)
+                                                    :type :source}})
+                         nil))
                       stop-chan
                       get-req-config
                       (assoc get-params :since get-since)
@@ -74,56 +88,44 @@
         ;; Post loop
         (a/go-loop []
           (println "post loop run")
-          (if-let [{:keys [statements
-                           errors
-                           cursor]
-                    :as event} (a/<! get-chan)]
-            (do (println (format "batch cursor %s" cursor))
-              (if (empty? errors)
-                (if (not-empty statements)
-                  ;; If we have statements, we'll attempt POST
-
-                  ;; DEBUG: the post code below seems to work fine for several
-                  ;; batches but then parks on put
-
-                  (do (println (format "POSTing %d statements and %d attachments"
-                                       (count statements)
-                                       (count (mapcat :attachments statements))))
-                      (store/update-job store id cursor [] nil)
-                      (recur))
-                  #_(let [_ (println (format "POSTing %d statements"
-                                           (count statements)))
-                        post-request (client/post-request
-                                      post-req-config
-                                      (mapv :statement statements)
-                                      (into [] (mapcat :attachments statements)))
-                        [tag x] (a/<! (client/async-request post-request))]
-                    (case tag
-                      ;; On success, update the cursor and keep listening
-                      :response
-                      (do (store/update-job store id cursor [] nil)
-                          (recur))
-                      ;; If the post fails, log the error and stop
-                      :exception
-                      (store/update-job
-                       store id nil
-                       [{:message (ex-message x)
-                         :type    :target}]
-                       nil)))
-                  ;; On an empty batch, Update cursor only
-                  ;; keep listening
-                  (do
+          (if-let [{:keys [status
+                           error]} (a/poll! stop-chan)]
+            ;; A stop is called!
+            (case status
+              :paused (store/update-job store id nil [] :paused)
+              :error (store/update-job store id nil [error] nil))
+            (if-let [batch (a/<! get-chan)]
+              (let [statements (mapv :statement batch)
+                    cursor (-> statements last (get "stored"))
+                    attachments (mapcat :attachments batch)]
+                (println (format "batch cursor %s" cursor))
+                (do (println (format "POSTing %d statements and %d attachments"
+                                     (count statements)
+                                     (count attachments)))
                     (store/update-job store id cursor [] nil)
-                    (recur)))
-                ;; Stop with errors
-                (do (println (format "Stopping with %d error(s)!"
-                                     (count errors)))
-                    (store/update-job store id nil errors nil))))
-            ;; Attempt to determine if this is complete or if the stop-chan has
-            ;; been called!
-            (store/update-job store id nil [] (if (a/poll! stop-chan)
-                                                :paused
-                                                :complete))))
+                    (recur))
+                #_(let [_ (println (format "POSTing %d statements"
+                                         (count statements)))
+                      post-request (client/post-request
+                                    post-req-config
+                                    (mapv :statement statements)
+                                    (into [] (mapcat :attachments statements)))
+                      [tag x] (a/<! (client/async-request post-request))]
+                  (case tag
+                    ;; On success, update the cursor and keep listening
+                    :response
+                    (do (store/update-job store id cursor [] nil)
+                        (recur))
+                    ;; If the post fails, log the error and stop
+                    :exception
+                    (store/update-job
+                     store id nil
+                     [{:message (ex-message x)
+                       :type    :target}]
+                     nil))))
+              ;; Job finishes naturally
+              (do (println "job finish")
+                  (store/update-job store id nil [] :complete)))))
         ;; Return the stop function
         stop-fn))))
 
