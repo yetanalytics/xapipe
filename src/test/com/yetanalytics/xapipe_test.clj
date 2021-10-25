bcd6c109a6e0cd8679a4ddb99fe840ac797f01cb
Milt Reder <milt@yetanalytics.com>
2021-10-25 13:45:37 -0400

    PIPE-14 added simple store test case

--- src/test/com/yetanalytics/xapipe_test.clj
+++ src/test/com/yetanalytics/xapipe_test.clj
@@ -4,13 +4,16 @@
             [clojure.tools.logging :as log]
             [com.yetanalytics.xapipe :refer :all]
             [com.yetanalytics.xapipe.job :as job]
-            [com.yetanalytics.xapipe.test-support :as support])
+            [com.yetanalytics.xapipe.store :as store]
+            [com.yetanalytics.xapipe.store.impl.memory :as mem]
+            [com.yetanalytics.xapipe.test-support :as support]
+            [com.yetanalytics.xapipe.util.time :as t])
   (:import [java.time Instant]))
 
 (use-fixtures :each (partial support/source-target-fixture
                              "dev-resources/lrs/after_conf.edn"))
 
-(deftest xfer-test
+(deftest run-job-test
   (testing "xapipe transfers conf test data from source to target"
     ;; Make sure it's in there
     (is (= 453 (support/lrs-count support/*source-lrs*)))
@@ -38,7 +41,7 @@
           all-states (a/<!! (a/go-loop [acc []]
                               (if-let [state (a/<! states)]
                                 (do
-                                  (log/info "state" state)
+                                  (log/debug "state" state)
                                   (recur (conj acc state)))
                                 acc)))]
       ;; At this point we're done or have errored.
@@ -56,3 +59,30 @@
           (let [source-idset (into #{} (support/lrs-ids support/*source-lrs*))]
             (is (every? #(contains? source-idset %)
                         (support/lrs-ids support/*target-lrs*)))))))))
+
+(deftest store-states-test
+  (testing "xapipe stores job state in the given state store"
+    (let [store (mem/new-store)
+          [since until] (support/lrs-stored-range support/*source-lrs*)
+          config {:source
+                  {:request-config (:request-config support/*source-lrs*)
+                   :get-params     {:since since
+                                    :until until}
+                   :poll-interval  1000
+                   :batch-size     50}
+                  :target
+                  {:request-config (:request-config support/*target-lrs*)
+                   :batch-size     50}}
+          job-id (.toString (java.util.UUID/randomUUID))
+          job (job/init-job
+               job-id
+               config)
+          {:keys [states]} (run-job job)]
+      (is (= (assoc job
+              :state {:status :complete
+                      :cursor (t/normalize-stamp until)
+                      :errors []
+                      :source {:errors []}
+                      :target {:errors []}})
+             (a/<!! (store-states states store))
+             (store/read-job store job-id))))))
