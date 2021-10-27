13aeae2665048773d4918377f65cee22c36e05d5
Milt Reder <milt@yetanalytics.com>
2021-10-27 12:04:09 -0400

    simple transfer test for cli

---
+++ src/test/com/yetanalytics/xapipe/main_test.clj
@@ -0,0 +1,21 @@
+(ns com.yetanalytics.xapipe.main-test
+  (:require [clojure.test :refer :all]
+            [com.yetanalytics.xapipe.main :refer :all]
+            [com.yetanalytics.xapipe.test-support :as support]))
+
+(use-fixtures :each (partial support/source-target-fixture
+                             "dev-resources/lrs/after_conf.edn"))
+
+(deftest start-test
+  (testing "start initializes and runs a job"
+    (let [[since until] (support/lrs-stored-range support/*source-lrs*)]
+      (is (-> (main* ;; we test this because it doesn't exit!
+               "start"
+               (format "http://0.0.0.0:%d/xapi"
+                       (:port support/*source-lrs*))
+               (format "http://0.0.0.0:%d/xapi"
+                       (:port support/*target-lrs*))
+               "-p" (format "since=%s" since)
+               "-p" (format "until=%s" until))
+              :status
+              (= 0))))))
