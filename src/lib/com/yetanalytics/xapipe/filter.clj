15532f96fb22ce12bec1af9d85df03cabeb99aed
Milt Reder <milt@yetanalytics.com>
2021-10-29 17:55:38 -0400

    naive attempt at transducer for persephone pattern filtering

--- src/lib/com/yetanalytics/xapipe/filter.clj
+++ src/lib/com/yetanalytics/xapipe/filter.clj
@@ -4,10 +4,15 @@
             [xapi-schema.spec :as xs]
             [cheshire.core :as json]
             [com.yetanalytics.persephone :as per]
+            [com.yetanalytics.persephone.pattern.fsm :as fsm]
+            [com.yetanalytics.persephone.pattern.fsm-spec :as fsm-spec]
+            [com.yetanalytics.persephone.template :as per-template]
             [com.yetanalytics.persephone.utils.json :as per-json]
+            [com.yetanalytics.pan.objects.pattern :as pat]
             [com.yetanalytics.pan.objects.profile :as prof]
             [com.yetanalytics.pan.objects.template :as template]
             [com.yetanalytics.xapipe.client.multipart-mixed :as mm]))
+
 
 (s/def ::profile-url string?) ;; These can be from disk, so don't spec 'em too hard
 (s/def ::template-id ::template/id)
@@ -73,6 +78,80 @@
                   v statement))
                validators)
          attachments)))))
+
+(s/def ::pattern-id ::pat/id)
+
+(s/def ::pattern-ids (s/every ::pattern-id))
+
+;; Pattern filter config
+(s/def ::pattern
+  (s/keys :req-un [::profile-urls
+                   ::pattern-ids]))
+
+(s/fdef pattern-filter-xf
+  :args (s/cat :pattern ::pattern
+               :fsm-state (s/nilable any?)
+               )
+  ;; Ret here is a transducer, TODO: spec it?
+  )
+
+(defn pattern-filter-xf
+  "Return a transducer that will filter a sequence of statements to only those
+  the given profiles' patterns, restricted to pattern-ids if provided."
+  [{:keys [profile-urls
+           pattern-ids]}
+   & [fsm-state]
+   ]
+  (let [fsm-map (-> profile-urls
+                    (->> (map get-profile)
+                         (map per/profile->fsms)
+                         (into {}))
+                    (cond->
+                        (not-empty pattern-ids)
+                      (select-keys pattern-ids)))]
+    (fn [xf]
+      (let [fsm-state-v (volatile! fsm-state)]
+        (fn
+          ([] (xf))
+          ([result]
+           (xf result))
+          ([result input]
+           (let [last-state @fsm-state-v
+                 ?reg (get-in input [:statement "context" "registration"])
+                 ?subreg (get-in input [:statement "context" "extensions" per/subreg-iri])
+                 ?state-key (cond
+                              (and ?reg ?subreg) [?reg ?subreg]
+                              ?reg ?reg
+                              :else nil)
+                 hits (into []
+                            (when ?state-key
+                              (for [[pat-key fsm] fsm-map
+                                    :let [{:keys [accepted?]
+                                           :as hit} (fsm/read-next fsm
+                                                                   (get last-state ?state-key)
+                                                                   (:statement input))]
+                                    :when accepted?]
+                                [?state-key pat-key hit])))]
+             ;; TODO: How are registrations removed from this?
+             ;; What is final completion?
+             (let [new-state (cond-> (reduce
+                                      (fn [m [state-key pat-key state]]
+                                        (assoc-in m [state-key pat-key] state))
+                                      last-state
+                                      hits)
+                               ;; remove any active if we've failed
+                               (and (empty? hits) ?state-key)
+                               (dissoc ?state-key))]
+               (vreset! fsm-state-v
+                        new-state)
+               (if (not-empty hits)
+                 (xf result (assoc input
+                                   :fsm-state new-state))
+                 (do
+                   ;; Drop the input. We must delete any attachments
+                   (when-let [attachments (not-empty (:attachments input))]
+                     (mm/clean-tempfiles! attachments))
+                   result))))))))))
 
 ;; Config map for all filtering
 (def filter-config-spec
--- src/test/com/yetanalytics/xapipe/filter_test.clj
+++ src/test/com/yetanalytics/xapipe/filter_test.clj
@@ -72,3 +72,97 @@
         [a-profile] 5 a-template-ids a-statements
         [b-profile] 1 b-template-ids b-statements
         [b-profile] 5 b-template-ids b-statements))))
+
+(deftest pattern-filter-xf-test
+  (let [a-profile "dev-resources/profiles/calibration_a.jsonld"
+        a-pattern-ids (-> a-profile get-profile :patterns (->> (mapv :id)))
+        a-statements (into []
+                           (support/gen-statements
+                            50 :profiles [a-profile]))
+
+        b-profile "dev-resources/profiles/calibration_b.jsonld"
+        b-pattern-ids (-> b-profile get-profile :patterns (->> (mapv :id)))
+        b-statements (into []
+                           (support/gen-statements
+                            50 :profiles [b-profile]))
+        all-statements (interleave a-statements b-statements)]
+    (testing "Finds statements"
+      (is (not-empty
+           (sequence (pattern-filter-xf
+                      {:profile-urls [a-profile]
+                       :pattern-ids []})
+                     (mapv
+                      (fn [s]
+                        {:statement s
+                         :attachments []})
+                      all-statements)))))
+
+    (testing "Passes fsm state"
+      (is (-> (sequence (pattern-filter-xf
+                         {:profile-urls [a-profile]
+                          :pattern-ids []})
+                        (mapv
+                         (fn [s]
+                           {:statement s
+                            :attachments []})
+                         all-statements))
+              last
+              :fsm-state
+              some?)))
+    (testing "Profile pattern filter filters by profile + pattern IDs"
+      (are [profile-urls pattern-ids statements]
+
+          (cset/subset?
+           (set
+            (map :statement
+                 (sequence (pattern-filter-xf
+                            {:profile-urls profile-urls
+                             :pattern-ids pattern-ids})
+                           (mapv
+                            (fn [s]
+                              {:statement s
+                               :attachments []})
+                            all-statements))))
+             (set statements))
+
+        ;; By profiles only
+        ;; Just A
+        [a-profile] [] a-statements
+        ;; Just B
+        [b-profile] [] b-statements
+        ;; Both
+        [a-profile b-profile] [] all-statements
+        ;; None
+        [] [] []
+        ;; All A pattern IDs
+        [a-profile] a-pattern-ids a-statements
+        ;; All B pattern IDs
+        [b-profile] b-pattern-ids b-statements
+        ;; All pattern IDs
+        [a-profile b-profile] (concat a-pattern-ids
+                                      b-pattern-ids) all-statements))
+    (testing "Filtering by single or multiple pattern IDs"
+      (are [profile-urls n-templates pattern-ids statements]
+          ;; For a given grouping of IDs, the resulting statements
+          ;; are a subset of the given profile
+          (every?
+           (fn [ids]
+             (cset/subset?
+              (into #{}
+                    (comp
+                     (pattern-filter-xf
+                      {:profile-urls profile-urls
+                       :pattern-ids ids})
+                     (map :statement))
+                    (mapv
+                     (fn [s]
+                       {:statement s
+                        :attachments []})
+                     all-statements))
+              (set statements)))
+           (partition-all n-templates pattern-ids))
+
+        [a-profile] 1 a-pattern-ids a-statements
+        [a-profile] 5 a-pattern-ids a-statements
+        [b-profile] 1 b-pattern-ids b-statements
+        [b-profile] 5 b-pattern-ids b-statements))))
