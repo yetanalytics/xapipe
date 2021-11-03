13a551699b5f002e7cd52763dad0248327bfa309
Milt Reder <milt@yetanalytics.com>
2021-11-02 20:03:33 -0400

    PIP-29 provide a way to dump state of mem store

--- src/lib/com/yetanalytics/xapipe/store/impl/memory.clj
+++ src/lib/com/yetanalytics/xapipe/store/impl/memory.clj
@@ -1,6 +1,10 @@
 (ns com.yetanalytics.xapipe.store.impl.memory
   (:require [com.yetanalytics.xapipe.store :as store]))
 
+(defprotocol DumpableMemoryStore
+  (dump [this]
+    "Reveal the store state"))
+
 (deftype MemoryStore [state-atom]
   store/XapipeStore
   (read-job [store job-id]
@@ -9,7 +13,10 @@
                      :as job}]
     (-> (swap! state-atom assoc job-id job)
         (get job-id)
-        (= job))))
+        (= job)))
+  DumpableMemoryStore
+  (dump [_]
+    @state-atom))
 
 (defn new-store
   "Make a new in-memory store"
