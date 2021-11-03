40761728104a9ccc8c05b5c0dab8ec89b33be354
Milt Reder <milt@yetanalytics.com>
2021-11-02 20:15:12 -0400

    PIP-29 force-resume test

--- src/test/com/yetanalytics/xapipe/main_test.clj
+++ src/test/com/yetanalytics/xapipe/main_test.clj
@@ -364,3 +364,92 @@
                   :status
                   (= 0)))
           (is (= 452 (sup/lrs-count target))))))))
+
+(deftest force-resume-test
+  (sup/with-running [source (sup/lrs
+                             :seed-path
+                             "dev-resources/lrs/after_conf.edn")
+                     target (sup/lrs)]
+    (let [[since until] (sup/lrs-stored-range source)
+          ;; not what we'd normally get
+          {:keys [states
+                  stop-fn
+                  store]}
+          (with-redefs [;; We hijack the internal handle-job
+                        ;; fn so we can see what happens
+                        handle-job (fn [store job client-opts]
+                                     (assoc
+                                      (xapipe/run-job job client-opts)
+                                      ;; Attach the store for possible
+                                      ;; inspection who knows
+                                      :store store))]
+            (main*
+             "--source-url" (format "http://0.0.0.0:%d/xapi"
+                                    (:port source))
+             ;; Pass a bad target
+             "--target-url" "http://0.0.0.0:1234/xapi"
+             "-p" (format "since=%s" since)
+             "-p" (format "until=%s" until)
+             ;; use an atom we can DUMP
+             "-s" "mem"))
+          ;; take 2 states so some communication might happen
+          _ (a/<!! (a/into [] (a/take 2 states)))
+          ;; Wait for it
+          _ (Thread/sleep 1000)
+          ;; What remains?
+          tail-states (a/<!! (a/into [] states))
+          job-id (-> tail-states first :id)]
+      (testing "make sure it is errored"
+        (is (= [:error]
+               (map #(get-in % [:state :status])
+                    tail-states))))
+      ;; Go ahead and stick it in the store now
+      (store/write-job store (last tail-states))
+      (is (= {job-id
+              {:id job-id,
+                :config
+                {:get-buffer-size 10,
+                 :statement-buffer-size 500,
+                 :batch-buffer-size 10,
+                 :batch-timeout 200,
+                 :source
+                 {:request-config
+                  {:url-base (format "http://0.0.0.0:%d"
+                                     (:port source)),
+                   :xapi-prefix "/xapi"},
+                  :get-params
+                  {:since "2021-10-25T15:05:00.537746000Z",
+                   :until "2021-10-25T15:05:32.595885000Z"},
+                  :poll-interval 1000,
+                  :batch-size 50,
+                  :backoff-opts {:budget 10000, :max-attempt 10}},
+                 :target
+                 {:request-config
+                  {:url-base "http://0.0.0.0:1234", :xapi-prefix "/xapi"},
+                  :batch-size 50,
+                  :backoff-opts {:budget 10000, :max-attempt 10}},
+                 :filter {}},
+                :state
+                {:status :error,
+                 :cursor "2021-10-25T15:05:00.537746000Z",
+                 :source {:errors []},
+                 :target {:errors [{:type :target, :message "Connection refused"}]},
+                 :errors [],
+                 :filter {}},
+                :get-buffer-size 10,
+                :batch-timeout 200}}
+             (mem/dump store)))
+      ;; Resume from cli
+      (testing "xapipe force resumes an errored job"
+        (let [job-id (-> tail-states last :id)]
+          (is (-> (with-redefs [create-store (constantly store)]
+                    (main*
+                     "--job-id" job-id
+                     "-s" "mem"
+                     "--target-url" (format "http://0.0.0.0:%d/xapi"
+                                            (:port target))
+                     "-f" ;; force arg
+                     ))
+                  :status
+                  (= 0)))
+          (is (= 452 (sup/lrs-count target))))))))
