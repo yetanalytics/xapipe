0179bf64beda4732411607256b3bb4573c453458
Milt Reder <milt@yetanalytics.com>
2021-11-03 10:02:16 -0400

    refactor cli helpers out to own ns

--- src/cli/com/yetanalytics/xapipe/main.clj
+++ src/cli/com/yetanalytics/xapipe/main.clj
@@ -1,323 +1,10 @@
 (ns com.yetanalytics.xapipe.main
-  (:require [clojure.core.async :as a]
-            [clojure.spec.alpha :as s]
-            [clojure.string :as cs]
-            [clojure.tools.cli :as cli]
-            [clojure.tools.logging :as log]
-            [cheshire.core :as json]
-            [com.yetanalytics.xapipe :as xapipe]
-            [com.yetanalytics.xapipe.client :as client]
-            [com.yetanalytics.xapipe.cli.options :as opts
-             :refer [common-options
-                     source-options
-                     target-options
-                     job-options]]
-            [com.yetanalytics.xapipe.job :as job]
+  (:require [clojure.tools.logging :as log]
+            [com.yetanalytics.xapipe.cli :as cli]
+            [com.yetanalytics.xapipe.cli.options :as opts]
             [com.yetanalytics.xapipe.job.state :as state]
-            [com.yetanalytics.xapipe.store :as store]
-            [com.yetanalytics.xapipe.store.impl.noop :as noop-store]
-            [com.yetanalytics.xapipe.store.impl.redis :as redis-store]
-            [com.yetanalytics.xapipe.store.impl.memory :as mem-store]
-            [xapi-schema.spec.resources :as xsr])
-  (:import [java.net URL])
+            [com.yetanalytics.xapipe.store :as store])
   (:gen-class))
-
-;; xAPI partial GET params
-;; TODO: Move spec
-(s/def ::partial-get-params
-  (s/keys :opt-un [:xapi.statements.GET.request.params/agent
-                   :xapi.statements.GET.request.params/verb
-                   :xapi.statements.GET.request.params/activity
-                   :xapi.statements.GET.request.params/registration
-                   :xapi.statements.GET.request.params/related_activities
-                   :xapi.statements.GET.request.params/related_agents
-                   :xapi.statements.GET.request.params/since
-                   :xapi.statements.GET.request.params/until
-                   :xapi.statements.GET.request.params/format]))
-
-;; helpers
-(defn create-store
-  [{:keys [storage
-           redis-host
-           redis-port]}]
-  (case storage
-    :noop (noop-store/new-store)
-    :redis (if (and redis-host redis-port)
-             (redis-store/new-store
-              ;; TODO: Pool?
-              {:pool {}
-               :spec
-               {:uri (format "redis://%s:%d"
-                             redis-host
-                             redis-port)}})
-             (throw (ex-info "Redis Config Required!"
-                             {:type ::redis-config-required})))
-    :mem (mem-store/new-store)))
-
-(defn parse-lrs-url
-  [^String url]
-  (try
-    (let [^URL parsed (URL. url)]
-      {:url-base (format
-                  "%s://%s"
-                  (.getProtocol parsed)
-                  (.getAuthority parsed))
-       :xapi-prefix (.getPath parsed)})
-    (catch Exception ex
-      (throw (ex-info (format "Could not parse LRS URL %s" url))))))
-
-(defn force-stop-job!
-  "Given a stop-fn and states channel, finish and stop the job.
-  *BLOCKING*"
-  [stop-fn states]
-  (when (stop-fn)
-    (a/<!! (a/into [] states))))
-
-(defn handle-job
-  "Actually execute a job, wrapping result
-  Redef this when testing for cooler output"
-  [store job client-opts]
-  (try
-    (let [{:keys [states]
-           stop :stop-fn} (xapipe/run-job job client-opts)]
-      (.addShutdownHook (Runtime/getRuntime)
-                        (Thread. ^Runnable
-                                 (fn []
-                                   (force-stop-job! stop states))))
-      (let [{{:keys [status]} :state
-             :as job-result} (-> states
-                                 (xapipe/log-states :info)
-                                 (xapipe/store-states store)
-                                 a/<!!)]
-        {:status (if (= :error status)
-                   1
-                   0)}))
-    (catch Exception ex
-      (log/error ex "Runtime Exception")
-      {:status 1
-       :message (ex-message ex)})))
-
-(defn options->client-opts
-  [{:keys [conn-timeout
-           conn-threads
-           conn-default-per-route
-           conn-insecure?
-           conn-io-thread-count]}]
-  {:conn-mgr-opts
-   (cond-> {}
-     conn-timeout (assoc :timeout conn-timeout)
-     conn-threads (assoc :threads conn-threads)
-     conn-default-per-route
-     (assoc :default-per-route conn-default-per-route)
-     conn-insecure? (assoc :insecure? conn-insecure?)
-     conn-io-thread-count
-     (assoc-in [:io-config :io-thread-count]
-               conn-io-thread-count))})
-
-(defn options->config
-  [{:keys [job-id
-           source-batch-size
-           source-poll-interval
-           get-params
-           source-username
-           source-password
-           source-backoff-budget
-           source-backoff-max-attempt
-           source-backoff-j-range
-           source-backoff-initial
-
-           target-batch-size
-           target-username
-           target-password
-           target-backoff-budget
-           target-backoff-max-attempt
-           target-backoff-j-range
-           target-backoff-initial
-
-           get-buffer-size
-           batch-timeout
-
-           filter-template-profile-urls
-           filter-template-ids
-           filter-pattern-profile-urls
-           filter-pattern-ids
-
-           statement-buffer-size
-           batch-buffer-size]}
-   source-req-config
-   target-req-config]
-  (cond-> {:get-buffer-size get-buffer-size
-           :batch-timeout batch-timeout
-           :source
-           {:request-config (cond-> source-req-config
-                              (and source-username
-                                   source-password)
-                              (assoc :username source-username
-                                     :password source-password))
-            :get-params     get-params
-            :poll-interval  source-poll-interval
-            :batch-size     source-batch-size
-            :backoff-opts
-            (cond-> {:budget source-backoff-budget
-                     :max-attempt source-backoff-max-attempt}
-              source-backoff-j-range
-              (assoc :j-range source-backoff-j-range)
-              source-backoff-initial
-              (assoc :initial source-backoff-initial))}
-           :target
-           {:request-config (cond-> target-req-config
-                              (and target-username
-                                   target-password)
-                              (assoc :username target-username
-                                     :password target-password))
-            :batch-size     target-batch-size
-            :backoff-opts
-            (cond-> {:budget target-backoff-budget
-                     :max-attempt target-backoff-max-attempt}
-              target-backoff-j-range
-              (assoc :j-range target-backoff-j-range)
-              target-backoff-initial
-              (assoc :initial target-backoff-initial))}
-           :filter {}}
-    statement-buffer-size
-    (assoc :statement-buffer-size statement-buffer-size)
-
-    batch-buffer-size
-    (assoc :batch-buffer-size batch-buffer-size)
-
-    (not-empty filter-template-profile-urls)
-    (assoc-in [:filter :template] {:profile-urls filter-template-profile-urls
-                                   :template-ids (into []
-                                                       filter-template-ids)})
-    (not-empty filter-pattern-profile-urls)
-    (assoc-in [:filter :pattern] {:profile-urls filter-pattern-profile-urls
-                                  :pattern-ids (into []
-                                                     filter-pattern-ids)})))
-
-(defn create-job
-  "Create a new job from options or throw"
-  [{:keys [source-url
-           target-url]
-    :as options}]
-  (when (empty? source-url)
-    (throw (ex-info "--source-lrs-url cannot be empty!"
-                    {:type ::no-source-url})))
-  (when (empty? target-url)
-    (throw (ex-info "--target-lrs-url cannot be empty!"
-                    {:type ::no-target-url})))
-  (let [source-req-config (parse-lrs-url source-url)
-        target-req-config (parse-lrs-url target-url)]
-    (cond
-      ;; invalid xapi params
-      (not (s/valid? ::partial-get-params
-                     (:get-params options)))
-      (throw (ex-info (str "invalid xAPI params:\n"
-                           (s/explain-str
-                            ::partial-get-params
-                            (:get-params options)))
-                      {:type :invalid-get-params}))
-      ;; Minimum required to try a job!
-      :else
-      (let [config (options->config
-                    options
-                    source-req-config
-                    target-req-config)
-            job-id (or (:job-id options)
-                       (.toString (java.util.UUID/randomUUID)))]
-        (job/init-job job-id config)))))
-
-(defn reconfigure-job
-  "Given an extant job and CLI options, apply any overriding options"
-  [job
-   {:keys [source-url
-           source-username
-           source-password
-
-           source-batch-size
-           source-poll-interval
-           get-params
-
-           target-url
-           target-username
-           target-password
-
-           target-batch-size
-
-           get-buffer-size
-           batch-timeout
-
-           statement-buffer-size
-           batch-buffer-size]}]
-  (cond-> job
-    source-url
-    (update-in
-     [:config :source :request-config]
-     merge (parse-lrs-url source-url))
-
-    source-username
-    (assoc-in
-     [:config :source :request-config :username]
-     source-username)
-
-    source-password
-    (assoc-in
-     [:config :source :request-config :password]
-     source-password)
-
-    ;; if there's a default, only update on change
-    (not= source-batch-size
-          (get-in job [:config :source :batch-size]))
-    (assoc-in
-     [:config :source :batch-size]
-     source-batch-size)
-
-    (not= source-poll-interval
-          (get-in job [:config :source :poll-interval]))
-    (assoc-in
-     [:config :source :poll-interval]
-     source-poll-interval)
-
-    ;; With no args, get-params is an empty map, so ignore
-    (and (not-empty get-params)
-         (not= get-params
-               (get-in job [:config :source :get-params])))
-    (assoc-in
-     [:config :source :get-params]
-     get-params)
-
-    target-url
-    (update-in
-     [:config :target :request-config]
-     merge (parse-lrs-url target-url))
-
-    target-username
-    (assoc-in
-     [:config :target :request-config :username]
-     target-username)
-
-    target-password
-    (assoc-in
-     [:config :target :request-config :password]
-     target-password)
-
-    target-batch-size
-    (assoc-in
-     [:config :target :batch-size]
-     target-batch-size)
-
-    (not= get-buffer-size
-          (get job :get-buffer-size))
-    (assoc :get-buffer-size get-buffer-size)
-
-    (not= batch-timeout
-          (get job :batch-timeout))
-    (assoc :batch-timeout batch-timeout)
-
-    statement-buffer-size
-    (assoc :statement-buffer-size statement-buffer-size)
-
-    batch-buffer-size
-    (assoc :batch-buffer-size batch-buffer-size)))
 
 (def usage
   "
@@ -346,11 +33,11 @@ Force Resume a job with errors:
                  usage
                  "All options:\n"
                  summary)}
-      (let [store (create-store options)
+      (let [store (cli/create-store options)
             [new? job] (if-some [extant (and ?job-id
                                              (store/read-job store ?job-id))]
                          [false extant]
-                         [true (create-job
+                         [true (cli/create-job
                                 options)])]
         (if new?
           (log/infof "Created new job %s: %s" (:id job) (pr-str job))
@@ -364,14 +51,14 @@ Force Resume a job with errors:
                "Starting job %s"
                "Resuming job %s")
              (:id job))
-            (handle-job store
-                        (cond-> (reconfigure-job job options)
-                          (and
-                           (not new?)
-                           force-resume?)
-                          (-> (update :state state/clear-errors)
-                              (update :state state/set-status :paused)))
-                        (options->client-opts options))))))))
+            (cli/handle-job store
+                            (cond-> (cli/reconfigure-job job options)
+                              (and
+                               (not new?)
+                               force-resume?)
+                              (-> (update :state state/clear-errors)
+                                  (update :state state/set-status :paused)))
+                            (cli/options->client-opts options))))))))
 
 (defn -main [& args]
   (let [{:keys [status message]}
--- src/test/com/yetanalytics/xapipe/main_test.clj
+++ src/test/com/yetanalytics/xapipe/main_test.clj
@@ -2,256 +2,11 @@
   (:require [clojure.test :refer :all]
             [clojure.core.async :as a]
             [com.yetanalytics.xapipe :as xapipe]
+            [com.yetanalytics.xapipe.cli :as cli]
             [com.yetanalytics.xapipe.main :refer :all]
             [com.yetanalytics.xapipe.store :as store]
             [com.yetanalytics.xapipe.store.impl.memory :as mem]
             [com.yetanalytics.xapipe.test-support :as sup]))
-
-(deftest create-store-test
-  (is (satisfies? store/XapipeStore (create-store {:storage :noop})))
-  (is (satisfies? store/XapipeStore (create-store {:storage :redis
-                                                   :redis-host "localhost"
-                                                   :redis-port 1234})))
-  (is (satisfies? store/XapipeStore (create-store {:storage :mem}))))
-
-(deftest parse-lrs-url-test
-  (is (= {:url-base "http://0.0.0.0:8080", :xapi-prefix "/xapi"}
-         (parse-lrs-url "http://0.0.0.0:8080/xapi"))))
-
-(deftest options->client-opts-test
-  (is (= {:conn-mgr-opts
-          {:timeout 10,
-           :threads 4,
-           :default-per-route 2,
-           :io-config {:io-thread-count 16}}}
-         (options->client-opts
-          {:conn-timeout 10
-           :conn-threads 4
-           :conn-default-per-route 2
-           :conn-insecure? false
-           :conn-io-thread-count 16}))))
-
-(deftest options->config-test
-  (is (= {:get-buffer-size 100,
-          :batch-timeout 200,
-          :source
-          {:request-config
-           {:url-base "http://0.0.0.0:8080",
-            :xapi-prefix "/xapi",
-            :username "foo",
-            :password "bar"},
-           :get-params {},
-           :poll-interval 1000,
-           :batch-size 50,
-           :backoff-opts
-           {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-          :target
-          {:request-config
-           {:url-base "http://0.0.0.0:8081",
-            :xapi-prefix "/xapi",
-            :username "foo",
-            :password "bar"},
-           :batch-size 50,
-           :backoff-opts
-           {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-          :filter {},
-          :statement-buffer-size 1000,
-          :batch-buffer-size 100}
-         (options->config
-          {:job-id "foo"
-
-           :source-batch-size 50
-           :source-poll-interval 1000
-           :get-params {}
-           :source-username "foo"
-           :source-password "bar"
-           :source-backoff-budget 1000
-           :source-backoff-max-attempt 10
-           :source-backoff-j-range 10
-           :source-backoff-initial 1
-
-           :target-batch-size 50
-           :target-username "foo"
-           :target-password "bar"
-           :target-backoff-budget 1000
-           :target-backoff-max-attempt 10
-           :target-backoff-j-range 10
-           :target-backoff-initial 1
-
-           :get-buffer-size 100
-           :batch-timeout 200
-
-           :filter-template-profile-urls []
-           :filter-template-ids []
-           :filter-pattern-profile-urls []
-           :filter-pattern-ids []
-
-           :statement-buffer-size 1000
-           :batch-buffer-size 100}
-          {:url-base "http://0.0.0.0:8080", :xapi-prefix "/xapi"}
-          {:url-base "http://0.0.0.0:8081", :xapi-prefix "/xapi"}))))
-
-(deftest create-job-test
-  (is (= {:id "foo",
-          :config
-          {:get-buffer-size 100,
-           :statement-buffer-size 1000,
-           :batch-buffer-size 100,
-           :batch-timeout 200,
-           :source
-           {:request-config
-            {:url-base "http://0.0.0.0:8080",
-             :xapi-prefix "/xapi",
-             :username "foo",
-             :password "bar"},
-            :get-params {:limit 50},
-            :poll-interval 1000,
-            :batch-size 50,
-            :backoff-opts
-            {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-           :target
-           {:request-config
-            {:url-base "http://0.0.0.0:8081",
-             :xapi-prefix "/xapi",
-             :username "foo",
-             :password "bar"},
-            :batch-size 50,
-            :backoff-opts
-            {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-           :filter {}},
-          :state
-          {:status :init,
-           :cursor "1970-01-01T00:00:00Z",
-           :source {:errors []},
-           :target {:errors []},
-           :errors [],
-           :filter {}}}
-         (create-job
-          {:job-id "foo"
-
-           :source-url "http://0.0.0.0:8080/xapi"
-           :source-batch-size 50
-           :source-poll-interval 1000
-           :get-params {}
-           :source-username "foo"
-           :source-password "bar"
-           :source-backoff-budget 1000
-           :source-backoff-max-attempt 10
-           :source-backoff-j-range 10
-           :source-backoff-initial 1
-
-           :target-url "http://0.0.0.0:8081/xapi"
-           :target-batch-size 50
-           :target-username "foo"
-           :target-password "bar"
-           :target-backoff-budget 1000
-           :target-backoff-max-attempt 10
-           :target-backoff-j-range 10
-           :target-backoff-initial 1
-
-           :get-buffer-size 100
-           :batch-timeout 200
-
-           :filter-template-profile-urls []
-           :filter-template-ids []
-           :filter-pattern-profile-urls []
-           :filter-pattern-ids []
-
-           :statement-buffer-size 1000
-           :batch-buffer-size 100}))))
-
-(deftest reconfigure-job-test
-  (is (= {:id "foo",
-          :config
-          {:get-buffer-size 100,
-           :statement-buffer-size 1000,
-           :batch-buffer-size 100,
-           :batch-timeout 200,
-           :source
-           {:request-config
-            {:url-base "http://0.0.0.0:8082",
-             :xapi-prefix "/xapi2",
-             :username "baz",
-             :password "quxx"},
-            :get-params {:format "exact"},
-            :poll-interval 3000,
-            :batch-size 100,
-            :backoff-opts
-            {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-           :target
-           {:request-config
-            {:url-base "http://0.0.0.0:8083",
-             :xapi-prefix "/xapi2",
-             :username "baz",
-             :password "quxx"},
-            :batch-size 100,
-            :backoff-opts
-            {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-           :filter {}},
-          :state
-          {:status :init,
-           :cursor "1970-01-01T00:00:00Z",
-           :source {:errors []},
-           :target {:errors []},
-           :errors [],
-           :filter {}},
-          :get-buffer-size 200,
-          :batch-timeout 300,
-          :statement-buffer-size 10000,
-          :batch-buffer-size 1000}
-         (reconfigure-job
-          {:id "foo",
-           :config
-           {:get-buffer-size 100,
-            :statement-buffer-size 1000,
-            :batch-buffer-size 100,
-            :batch-timeout 200,
-            :source
-            {:request-config
-             {:url-base "http://0.0.0.0:8080",
-              :xapi-prefix "/xapi",
-              :username "foo",
-              :password "bar"},
-             :get-params {:limit 50},
-             :poll-interval 1000,
-             :batch-size 50,
-             :backoff-opts
-             {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-            :target
-            {:request-config
-             {:url-base "http://0.0.0.0:8081",
-              :xapi-prefix "/xapi",
-              :username "foo",
-              :password "bar"},
-             :batch-size 50,
-             :backoff-opts
-             {:budget 1000, :max-attempt 10, :j-range 10, :initial 1}},
-            :filter {}},
-           :state
-           {:status :init,
-            :cursor "1970-01-01T00:00:00Z",
-            :source {:errors []},
-            :target {:errors []},
-            :errors [],
-            :filter {}}}
-          {:job-id "foo"
-           :source-url "http://0.0.0.0:8082/xapi2"
-           :source-batch-size 100
-           :source-poll-interval 3000
-           :get-params {:format "exact"}
-           :source-username "baz"
-           :source-password "quxx"
-
-           :target-url "http://0.0.0.0:8083/xapi2"
-           :target-batch-size 100
-           :target-username "baz"
-           :target-password "quxx"
-
-           :get-buffer-size 200
-           :batch-timeout 300
-
-           :statement-buffer-size 10000
-           :batch-buffer-size 1000}))))
 
 (deftest start-test
   (sup/with-running [source (sup/lrs
@@ -283,12 +38,12 @@
                   store]}
           (with-redefs [;; We hijack the internal handle-job
                         ;; fn so we can see what happens
-                        handle-job (fn [store job client-opts]
-                                     (assoc
-                                      (xapipe/run-job job client-opts)
-                                      ;; Attach the store for possible
-                                      ;; inspection who knows
-                                      :store store))]
+                        cli/handle-job (fn [store job client-opts]
+                                         (assoc
+                                          (xapipe/run-job job client-opts)
+                                          ;; Attach the store for possible
+                                          ;; inspection who knows
+                                          :store store))]
             (main*
              "--source-url" (format "http://0.0.0.0:%d/xapi"
                                     (:port source))
@@ -357,7 +112,7 @@
         (let [job-id (-> tail-states last :id)]
           ;; Here we override create-store to return the same store
           ;; thus simultating proper persistence like redis
-          (is (-> (with-redefs [create-store (constantly store)]
+          (is (-> (with-redefs [cli/create-store (constantly store)]
                     (main*
                      "--job-id" job-id
                      "-s" "mem"))
@@ -377,12 +132,12 @@
                   store]}
           (with-redefs [;; We hijack the internal handle-job
                         ;; fn so we can see what happens
-                        handle-job (fn [store job client-opts]
-                                     (assoc
-                                      (xapipe/run-job job client-opts)
-                                      ;; Attach the store for possible
-                                      ;; inspection who knows
-                                      :store store))]
+                        cli/handle-job (fn [store job client-opts]
+                                         (assoc
+                                          (xapipe/run-job job client-opts)
+                                          ;; Attach the store for possible
+                                          ;; inspection who knows
+                                          :store store))]
             (main*
              "--source-url" (format "http://0.0.0.0:%d/xapi"
                                     (:port source))
@@ -407,39 +162,39 @@
       (store/write-job store (last tail-states))
       (is (= {job-id
               {:id job-id,
-                :config
-                {:get-buffer-size 10,
-                 :statement-buffer-size 500,
-                 :batch-buffer-size 10,
-                 :batch-timeout 200,
-                 :source
-                 {:request-config
-                  {:url-base (format "http://0.0.0.0:%d"
-                                     (:port source)),
-                   :xapi-prefix "/xapi"},
-                  :get-params
-                  {:since "2021-10-25T15:05:00.537746000Z",
-                   :until "2021-10-25T15:05:32.595885000Z"},
-                  :poll-interval 1000,
-                  :batch-size 50,
-                  :backoff-opts {:budget 10000, :max-attempt 10}},
-                 :target
-                 {:request-config
-                  {:url-base "http://0.0.0.0:1234", :xapi-prefix "/xapi"},
-                  :batch-size 50,
-                  :backoff-opts {:budget 10000, :max-attempt 10}},
-                 :filter {}},
-                :state
-                {:status :error,
-                 :cursor "2021-10-25T15:05:00.537746000Z",
-                 :source {:errors []},
-                 :target {:errors [{:type :target, :message "Connection refused"}]},
-                 :errors [],
-                 :filter {}},
-                :get-buffer-size 10,
-                :batch-timeout 200}}
+               :config
+               {:get-buffer-size 10,
+                :statement-buffer-size 500,
+                :batch-buffer-size 10,
+                :batch-timeout 200,
+                :source
+                {:request-config
+                 {:url-base (format "http://0.0.0.0:%d"
+                                    (:port source)),
+                  :xapi-prefix "/xapi"},
+                 :get-params
+                 {:since "2021-10-25T15:05:00.537746000Z",
+                  :until "2021-10-25T15:05:32.595885000Z"},
+                 :poll-interval 1000,
+                 :batch-size 50,
+                 :backoff-opts {:budget 10000, :max-attempt 10}},
+                :target
+                {:request-config
+                 {:url-base "http://0.0.0.0:1234", :xapi-prefix "/xapi"},
+                 :batch-size 50,
+                 :backoff-opts {:budget 10000, :max-attempt 10}},
+                :filter {}},
+               :state
+               {:status :error,
+                :cursor "2021-10-25T15:05:00.537746000Z",
+                :source {:errors []},
+                :target {:errors [{:type :target, :message "Connection refused"}]},
+                :errors [],
+                :filter {}},
+               :get-buffer-size 10,
+               :batch-timeout 200}}
              (mem/dump store)))
-      (with-redefs [create-store (constantly store)]
+      (with-redefs [cli/create-store (constantly store)]
         (testing "xapipe normally can't resume with errors"
           (is (= 1
                  (:status
