6e87a8318e705d3d2e6cd8ef3f682ce37f7e7d17
Milt Reder <milt@yetanalytics.com>
2021-11-18 12:15:31 -0500

    PIP-54 #in-progress delete any extra attachments

--- src/test/com/yetanalytics/xapipe/xapi_test.clj
+++ src/test/com/yetanalytics/xapipe/xapi_test.clj
@@ -2,8 +2,20 @@
   (:require [clojure.test :refer :all]
             [com.yetanalytics.xapipe.xapi :refer :all]
             [com.yetanalytics.xapipe.test-support :as sup]
-            [clojure.spec.test.alpha :as st]))
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sgen])
+  (:import [java.io File]))
 
 (sup/def-ns-check-tests
   com.yetanalytics.xapipe.xapi
   {:default {sup/stc-opts {:num-tests 10}}})
+
+(deftest response->statements-test
+  (testing "deletes extra attachments"
+    (let [attachment
+          (sgen/generate
+           (s/gen :com.yetanalytics.xapipe.client.multipart-mixed/attachment))]
+      (response->statements {:body
+                             {:statement-result {:statements []}
+                              :attachments [attachment]}})
+      (is (false? (.exists (:tempfile attachment)))))))
