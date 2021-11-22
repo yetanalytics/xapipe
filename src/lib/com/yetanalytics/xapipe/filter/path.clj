5d918da8de3ff2b9af8701165249a90dba590ff3
Milt Reder <milt@yetanalytics.com>
2021-11-22 13:59:11 -0500

    PIP-56 path parse fn

--- src/lib/com/yetanalytics/xapipe/filter/path.clj
+++ src/lib/com/yetanalytics/xapipe/filter/path.clj
@@ -1,11 +1,35 @@
 (ns com.yetanalytics.xapipe.filter.path
   "Simple JsonPath-based filtering to ensure dense data."
   (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sgen]
             [com.yetanalytics.pathetic :as path]
             [com.yetanalytics.pathetic.json-path :as jp]
             [xapi-schema.spec :as xs]))
 
 (s/def ::path ::jp/path)
+
+(s/fdef parse-path
+  :args (s/cat :path-str
+               (s/with-gen string?
+                 (fn []
+                   (sgen/elements
+                    ["$.id"
+                     "$.timestamp"
+                     "$.object.definition.type"
+                     "$.result.duration"
+                     "$.result.success"
+                     "$.result.completion"
+                     "$.result['success', 'completion']"
+                     "$.context.contextActivities.category[*].id"
+                     "$"
+                     "$.foo"]))))
+  :ret ::path)
+
+(defn parse-path
+  "Parse a JsonPath path, ignore any additional paths"
+  [path-str]
+  (first
+   (path/parse-paths path-str)))
 
 (s/def ::ensure-paths (s/every ::path))
 
