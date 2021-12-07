461061afcdfd32be0b912d889763d3af163cf15e
Milt Reder <milt@yetanalytics.com>
2021-12-07 15:17:19 -0500

    PIP-66 failing auto-tests show we didn't cover everything

---
+++ src/test/com/yetanalytics/xapipe/job/json_test.clj
@@ -0,0 +1,7 @@
+(ns com.yetanalytics.xapipe.job.json-test
+  (:require [com.yetanalytics.xapipe.job.json :refer :all]
+            [clojure.test :refer :all]
+            [com.yetanalytics.xapipe.test-support :as sup]))
+
+(sup/def-ns-check-tests
+  com.yetanalytics.xapipe.job.json)
